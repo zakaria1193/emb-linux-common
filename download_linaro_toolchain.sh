d28d2fa0f99e52c014defec481ab7c68a120dc6d
Zakaria Fadli <zakaria.fadli@netatmo.com>
2020-12-07 00:58:26 +0100

    linaro toolchain

---
+++ download_linaro_toolchain.sh
@@ -0,0 +1,3 @@
+wget -c https://releases.linaro.org/components/toolchain/binaries/6.5-2018.12/arm-linux-gnueabihf/gcc-linaro-6.5.0-2018.12-x86_64_arm-linux-gnueabihf.tar.xz
+tar xf gcc-linaro-6.5.0-2018.12-x86_64_arm-linux-gnueabihf.tar.xz
+export CC=`pwd`/gcc-linaro-6.5.0-2018.12-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
