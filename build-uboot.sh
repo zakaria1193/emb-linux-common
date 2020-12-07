bbff4ab80ce7a83bd5a07aa2673678a1a34f2b93
Zakaria Fadli <zakaria.fadli@netatmo.com>
2020-12-07 01:56:03 +0100

    add build uboot script

---
+++ build-uboot.sh
@@ -0,0 +1,3 @@
+export PATH=./gcc-linaro-6.5.0-2018.12-x86_64_arm-linux-gnueabihf/bin:$PATH
+make -C u-boot CROSS_COMPILE=arm-linux-gnueabihf- am335x_boneblack_defconfig
+make -C u-boot CROSS_COMPILE=arm-linux-gnueabihf-
